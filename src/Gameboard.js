f5ef630e8a50aff116a39d880f562172a2204e39
Chris Hendee <c.w.hendee@gmail.com>
2019-06-16 11:28:30 -0400

    Randomize maze layout at beginning of game

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -9,26 +9,52 @@ class Gameboard extends React.Component {
 
   constructor(props) {
     super(props);
-    this.initialize(this.props.numPlayers);
+    this.state = {
+      maze: this.randomizeMaze(),
+      tokens: this.placePlayerTokens(this.props.numPlayers)
+    };
   }
 
-  maze = [];
-  tokens = [];
+  randomizeMaze() {
+    // Add static tiles: the 4 corners and the center of the maze
+    let maze = [{id: 0, x:0, y:0, value: 6}, {id: 1, x:8*50, y:0, value: 3}, {id: 2, x:8*50, y:8*50, value: 9}, {id: 3, x:0, y:8*50, value: 12}, {id: 4, x: 4*50, y: 4*50, value: 0}];
+    
+    let shuffle = (array) => { return array.sort(() => Math.random() - 0.5)};
+    
+    // Generate list of randomized tiles to be placed into the maze
+    let tiles = [
+      3, 3, 3, 3, 3, 3, 3,
+      6, 6, 6, 6, 6, 6, 6,
+      9, 9, 9, 9, 9, 9, 9,
+      12, 12, 12, 12, 12, 12, 12,
+      1, 1, 1, 1, 1, 1, 1, 1,
+      2, 2, 2, 2, 2, 2, 2, 2,
+      4, 4, 4, 4, 4, 4, 4, 4,
+      8, 8, 8, 8, 8, 8, 8, 8,
+      5, 5, 5, 5, 5, 5, 5, 5,
+      10, 10, 10, 10, 10, 10, 10, 10
+    ];
+    
+    tiles = shuffle(tiles);
 
-  initialize(numPlayers) {
     // Generate a 9 by 9 array to represent the maze
-    let id = 0;
+    let id = 5;
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
-        this.maze.push({id:id, x: i*50, y:j*50, value:0})
-        id++;
+        // Place a tile at the current position if there isn't already one there
+        if (!maze.some(m => { return m.x === i*50 && m.y === j*50})) {
+          maze.push({id:id, x: i*50, y:j*50, value: tiles.pop()});
+          id++;
+        }
       }
     }
 
-    // Create the tokens for each player at their default starting position
-    for (let p = 0; p < numPlayers; p++) {
-      this.tokens.push({id:p+1, x:p%2 * 400 + 22, y:Math.floor(p/2) * 400 + 22});
-    }
+    return maze;
+  }
+
+  placePlayerTokens(numPlayers) {
+    let tokens = [{id: 1, x: 22, y: 22}, {id: 2, x: 422, y: 22}, {id: 3, x: 22, y: 422}, {id: 4, x: 422, y: 422}];
+    return tokens.slice(0, numPlayers);
   }
 
   // Shift a row or column in the maze by the specified number of squares
@@ -44,8 +70,8 @@ class Gameboard extends React.Component {
   // If a player clicks on a valid square, move their token to that position and end their turn
   onClick = (id) => {
 
-    let clickedSquare = this.maze.find((e) => { return e.id === id});
-    let playerToken = this.tokens.find((e) => { return e.id === this.props.currentPlayer });
+    let clickedSquare = this.state.maze.find((e) => { return e.id === id});
+    let playerToken = this.state.tokens.find((e) => { return e.id === this.props.currentPlayer });
 
     if (playerToken && clickedSquare) {
       playerToken.x = clickedSquare.x + 22;
@@ -59,8 +85,8 @@ class Gameboard extends React.Component {
     return (
       <div id="Gameboard">
         <svg className="maze" width={this.props.width} height={this.props.height}>
-          {this.maze.map(s => <Square key={s.id} {...s} onClick={this.onClick}/>)}
-          {this.tokens.map(t => <Token key={t.id} {...t} />)}
+          {this.state.maze.map(s => <Square key={s.id} {...s} onClick={this.onClick}/>)}
+          {this.state.tokens.map(t => <Token key={t.id} {...t} />)}
         </svg>
       </div>
     );
--- src/Square.js
+++ src/Square.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './Square.css';
 
-const Square = (props) => <rect className="open square" x={props.x} y={props.y} width="45" height="45" onClick={() => props.onClick(props.id)}></rect>
+const Square = (props) => (
+
+  <g className="open" onClick={() => props.onClick(props.id)}>
+    <rect className="square" x={props.x} y={props.y} width="45" height="45" />
+    <path className="mazePassage" d={
+      `M${props.x} ${props.y} m11 11
+      ${props.value & 4 ? 'v23' : 'h-10 v23 h10'}
+      ${props.value & 8 ? 'h23' : 'v10 h23 v-10'}
+      ${props.value & 1 ? 'v-23' : 'h10 v-23 h-10'}
+      ${props.value & 2 ? 'h-23' : 'v-10 h-23 v10'}
+      Z`
+    } />
+  </g>
+);
 
 
 export default Square;
